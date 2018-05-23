44c1e35824495ab950d3f58242a7e6e9586e4c0c
shushantkumar <shushantkmr2@gmail.com>
2018-05-23 11:29:13 +0530

    multichat

---
+++ multichat/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/shushant/Documents/Rubit/channels-examples-master/multichat/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
